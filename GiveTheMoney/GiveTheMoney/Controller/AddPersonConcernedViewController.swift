abb93deadaebe6c89e06f259980bfb6258f75222
Raphael LUCIANO <raphael.luciano@igtp4-mac02.isim.intra>
2019-04-02 19:10:30 +0200

    add "addConcernedPersonViewController"

---
+++ GiveTheMoney/GiveTheMoney/Controller/AddPersonConcernedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddPersonConcernedViewController.swift
+//  GiveTheMoney
+//
+//  Created by Raphael LUCIANO on 02/04/2019.
+//  Copyright © 2019 Raphael LUCIANO. All rights reserved.
+//
+
+import UIKit
+
+class AddPersonConcernedViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
